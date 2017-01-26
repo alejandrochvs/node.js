a87dafcae0a3fa656e8b38102ab5a67958ece061
Alejandro Chaves <alejandrochvs@hotmail.com>
2017-01-26 15:30:07 -0500

    Variables

--- hola_express/app.js
+++ hola_express/app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var hola = "Hola mundoo!"
 app.set('view engine', 'jade');
 app.get("/", function (req, res) {
     res.render("index", {
-        hola: "caa"
+        hola: hola
     });
 })
 app.listen(8080);
